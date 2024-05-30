a35b6aee297edf78ed4113d3d72e4caffeb5a8fe
Your Name <Your Email>
2024-05-30 19:37:35 -0400

    phase 2 and bonus complete

--- aa03-guessing-game/guessing-game-project/guessing-game.js
+++ aa03-guessing-game/guessing-game-project/guessing-game.js
@@ -5,8 +5,9 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-const secretNumber = randomInRange(0, 100);
-
+let secretNumber = null;
+let maxAttempts = null;
+let attempts = 0;
 function checkGuess(num) {
     if(num > secretNumber){
         console.log("Too high!");
@@ -24,10 +25,17 @@ function askGuess(){
     rl.question("Enter a guess: ", (answer) =>{
         const guess = Number(answer);
         const result = checkGuess(guess);
+        attempts++;
         if(result === true){
             rl.close()
-        }else{
+        }else if(attempts < maxAttempts){
+            console.log("You have " + (maxAttempts-attempts) + " left")
             askGuess();
+
+        }else if(attempts >= maxAttempts){
+            console.log("You have reached the maximum number of attempts");
+
+            rl.close();
         }
     });
 }
@@ -39,4 +47,27 @@ function randomInRange(min, max){
 
 }
 
+function askRange(){
+     rl.question("Enter a minimum number: ", (minNum) =>{
+        const minVal = Number(minNum);
+
+        rl.question("Enter a max number: ", (maxNum)=>{
+            const maxVal = Number(maxNum)
+            secretNumber = randomInRange(minVal, maxVal)
+            console.log("I'm thinking of a number between " + minVal + " and " + maxVal);
+
+            askGuess();
+
+        });
+    });
+}
+function askLimit(){
+    rl.question("Enter a max number of attempts: ", (att) =>{
+        maxAttempts = Number(att);
+        askRange();
+    });
+}
+
 // askGuess();
+// askRange();
+askLimit();
